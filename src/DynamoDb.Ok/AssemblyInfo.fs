d840d23e41bdcea0663d94bf0e5cb839dde94ea7
Blair55 <blair.nicholas@googlemail.com>
2020-04-16 17:56:39 +0100

    Bump version to 0.0.2
    ## [0.0.2] - 2020-04-16
    
    ### Added
    
    - Nothing

--- src/DynamoDb.Ok/AssemblyInfo.fs
+++ src/DynamoDb.Ok/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("DynamoDb.Ok")>]
 [<assembly: AssemblyProductAttribute("DynamoDb.Ok")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-04-16T00:00:00.0000000+01:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.2")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","f2ca2de0553112945b2cc9f37f814a12b37fa922")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "DynamoDb.Ok"
     let [<Literal>] AssemblyProduct = "DynamoDb.Ok"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-04-16T00:00:00.0000000+01:00"
+    let [<Literal>] AssemblyFileVersion = "0.0.2"
+    let [<Literal>] AssemblyInformationalVersion = "0.0.2"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "f2ca2de0553112945b2cc9f37f814a12b37fa922"
